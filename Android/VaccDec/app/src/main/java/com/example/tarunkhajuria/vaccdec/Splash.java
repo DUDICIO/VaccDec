4e256d4517585d4edd83ff6a1c9d8f08120ef282
tarunkhajuria42 <tarunkhajuria42@gmail.com>
2017-04-18 19:50:10 +0530

    Added Splash

---
+++ Android/VaccDec/app/src/main/java/com/example/tarunkhajuria/vaccdec/Splash.java
@@ -0,0 +1,28 @@
+package com.example.tarunkhajuria.vaccdec;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ImageView imgview=(ImageView)findViewById(R.id.imageView3);
+        imgview.setScaleType(ImageView.ScaleType.FIT_XY);
+    }
+
+
+
+}
