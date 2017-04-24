53e864a56907488a4a4e0e6712dd604b0944678a
tarunkhajuria42 <tarunkhajuria42@gmail.com>
2017-04-24 16:37:35 +0530

    Bluetooth Connection Service Added

--- Android/VaccDec/app/src/main/java/com/example/tarunkhajuria/vaccdec/Main.java
+++ Android/VaccDec/app/src/main/java/com/example/tarunkhajuria/vaccdec/Main.java
@@ -2,6 +2,7 @@ package com.example.tarunkhajuria.vaccdec;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
@@ -14,44 +15,57 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter mBluetoothAdapter;
     private List<BluetoothDevice> devicelist= new ArrayList<BluetoothDevice>();
+    private ArrayAdapter<String> madapter;
+    private BluetoothDevice selectedDevice;
+    private TextView sname;
+    private TextView connect;
+    private ImageView connImg;
+    BluetoothSocket msocket;
+    private Context activitycontext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activitycontext=this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //List view for Bluetooth Devices
         ListView btlist= (ListView) findViewById(R.id.devicesltview);
 
-        ArrayAdapter<String> madapter= new ArrayAdapter<String>(this,R.layout.text);
-        madapter.add("BT1");
-        madapter.add("BT2");
-        Log.d("Listview","Count:"+madapter.getCount());
+        madapter= new ArrayAdapter<String>(this,R.layout.text);
         btlist.setAdapter(madapter);
-        madapter.add("Adasd");
-        btlist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        btlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedDevice=devicelist.get(position);
+                sname.setText(selectedDevice.getName());
+                connect.setVisibility(TextView.VISIBLE);
             }
         });
-        //Bluetooth Connection
+
+        //Selected Device Handling
+        sname= (TextView) findViewById(R.id.sDevice);
+        connect=(TextView)findViewById(R.id.connect);
+
+
+        //Bluetooth Search
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
         } else {
@@ -61,17 +75,44 @@ public class Main extends AppCompatActivity {
             }
             else{
                 mBluetoothAdapter.startDiscovery();
+                Log.d("Bluetooth","Start Discovery");
             }
         }
+        //Bluetooth Connect
+        connImg=(ImageView)findViewById(R.id.vaccdecimg);
+        connImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                     msocket = selectedDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                }
+                catch(Exception e)
+                {
+                    Log.e("Bluetooth","Socket Not given:"+e);
+                }
+                try{
+                    msocket.connect();
+                }catch(IOException e)
+                {
+                    Log.e("Bluetooth","Could not connect with error:"+e);
+                }
+                Intent stacitivty=new Intent(activitycontext,BluetoothService.class);
+                
 
+
+            }
+        });
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_ENABLE_BT) {
-            if (requestCode == Activity.RESULT_OK) {
+                Log.d("Bluetooth","Here");
+            if (resultCode == Activity.RESULT_OK) {
+                Log.d("Bluetooth","Start Discovery");
                 mBluetoothAdapter.startDiscovery();
+
 
             }
         }
@@ -86,11 +127,8 @@ public class Main extends AppCompatActivity {
                 String deviceHarwareAdress = device.getAddress();
                 String deviceName = device.getName();
                 Log.d("Bluetooth",deviceName);
-                if(deviceName.contains("Vacc"))
-                {
-                    devicelist.add(device);
-                }
-
+                madapter.add(deviceName);
+                devicelist.add(device);
             }
         }
     };
